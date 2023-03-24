87e7772234a0c68e4e2af342eb0f8048c6e4c101
kaplosh <kaplanekt@gmail.com>
2023-03-24 11:39:34 +0100

    input components init

---
+++ showcase/components/InputFieldDisabled.vue
@@ -0,0 +1,13 @@
+<template>
+  <input type="text" disabled="disabled">
+</template>
+
+<script>
+export default {
+  name: "InputFieldDisabled",
+};
+</script>
+
+<style scoped>
+
+</style>
---
+++ showcase/components/InputFieldPlaceholder.vue
@@ -0,0 +1,13 @@
+<template>
+  <input type="text" placeholder="Placeholder">
+</template>
+
+<script>
+export default {
+  name: "InputFieldPlaceholder",
+};
+</script>
+
+<style scoped>
+
+</style>
