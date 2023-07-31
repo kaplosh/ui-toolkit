f7dea03fce3ec046fe7f6c0ea593d9163433ab26
kaplosh <kaplanekt@gmail.com>
2023-07-31 13:46:57 +0200

    DateSet prototype

--- lib/ui/controls/DateSet.vue
+++ lib/ui/controls/DateSet.vue
@@ -6,24 +6,51 @@ import { recordsMonths } from '../mocks.ts';
 import { recordsYears } from '../mocks.ts';
 
 
+
 const months: ui.OptionItem[] = recordsMonths.map(record => ({
   value: record.id,
   object: record,
 }));
 
-const days: ui.OptionItem[] = recordsDays.map(record => ({
-  value: record.id,
-  object: record,
-}));
+  const days1: ui.OptionItem[] = recordsDays[0].map(record => ({
+    value: record.id,
+    object: record,
+  }
+));
+  const days2: ui.OptionItem[] = recordsDays[1].map(record => ({
+    value: record.id,
+    object: record,
+  }
+));
+const days3: ui.OptionItem[] = recordsDays[2].map(record => ({
+    value: record.id,
+    object: record,
+  }
+));
 
 const years: ui.OptionItem[] = recordsYears.map(record => ({
   value: record.id,
   object: record,
 }));
-
-const select1 = ref(days.filter(item => Number(item.object.id) === 1));
+const currentDays = ref(days1);
+const select1 = ref(currentDays.value.filter(item => Number(item.object.id) === 1));
 const select2 = ref(months.filter(item => Number(item.object.id) === 1));
 const select3 = ref(years.filter(item => Number(item.object.id) === 1));
+
+function setDays(){
+  const longMonths = [ '1','3','5','7','8','10','12' ];
+  const currentMonth = select2.value[0].object.id;
+  if(longMonths.includes(currentMonth)) {
+    currentDays.value = days3;
+  } else if (currentMonth==='2') {
+    currentDays.value = days1;
+  } else {
+    currentDays.value = days2;
+  }
+}
+
+
+
 </script>
 
 <template>
@@ -32,7 +59,7 @@ const select3 = ref(years.filter(item => Number(item.object.id) === 1));
       <div class="col-md-6 col-lg-4 col-xl-3">
         <h2>Day select</h2>
         <ui.controls.OptionsSelectDropdown
-          :items="days"
+          :items="currentDays"
           :selected="select1"
           :single="true"
           @change="select1 = $event"
@@ -67,7 +94,8 @@ const select3 = ref(years.filter(item => Number(item.object.id) === 1));
           :items="months"
           :selected="select2"
           :single="true"
-          @change="select2 = $event"
+          @change="select2 = $event;"
+          @click="setDays()"
         >
           <template #selected="{ items }">
             <span
@@ -94,7 +122,7 @@ const select3 = ref(years.filter(item => Number(item.object.id) === 1));
         </ui.controls.OptionsSelectDropdown>
       </div>
       <div class="col-md-6 col-lg-4 col-xl-3">
-        <h2>Month select</h2>
+        <h2>Year select</h2>
         <ui.controls.OptionsSelectDropdown
           :items="years"
           :selected="select3"
