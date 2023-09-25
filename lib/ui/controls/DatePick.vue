61961a9e9ed7b282943de4c7f692f3594757683c
kaplosh <kaplanekt@gmail.com>
2023-09-25 12:48:37 +0200

    DateSet variants

---
+++ lib/ui/controls/DatePick.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import Datepicker from 'vue3-datepicker';
+import { ref } from 'vue';
+
+const datePicked = ref(new Date());
+
+const emits = defineEmits([ 'onChange' ]);
+
+function onChange () {
+  emits('onChange', datePicked);
+}
+
+</script>
+
+<template>
+  <Datepicker
+    v-model="datePicked"
+    typeable
+    @change="onChange"
+  />
+</template>
+
+<style scoped>
+
+</style>
